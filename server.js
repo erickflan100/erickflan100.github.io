fd1e29dc1522f311aaadf793a9b218ceb2178167
Erick Flanklin <erickflan10@yahoo.com>
2023-06-04 13:15:42 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs')
     
 async function getBible(l) {
     const list = []
-    const url = `https://search.nepebrasil.org/referencia-espirita?livro=1&chapter=${l}`
+    const url = `https://search.nepebrasil.org/referencia-espirita?livro=66&chapter=${l}`
     const {data} = await axios(url)
     const $ = cheerio.load(data)
 
@@ -17,14 +17,45 @@ async function getBible(l) {
             list.push(b)
         // }
     })
-    fs.writeFile(`Genesis${l}.json`, JSON.stringify(list), err => {
+    fs.writeFile(`Apocalipse${l}.json`, JSON.stringify(list), err => {
         if (err) console.error(err);
     });
     // console.log(list);
 }
+
 setTimeout(() => {
-for (var i = 1; i <= 50; i++){
+for (var i = 1; i <= 22; i++){
             getBible(i)
             console.log(i)
         }
 }, 5000)
+ 
+// fazer pesquisa sobre o comentário
+// async function getComment(l) {
+//     const list = []
+//     const url = `https://search.nepebrasil.org/referencia-espirita?livro=1&chapter=${l}`
+//     const {data} = await axios(url)
+//     const $ = cheerio.load(data)
+
+    
+//     $(".nepe-comment").each((i, elem) => {
+//         const title = $(elem).find('h4').text().trim()
+//         const vers = $(elem).find('.small').text().trim()
+//         const text = $(elem).text().trim()
+//         // if (vers !== "") {
+//             const b = { title, vers, text }
+//             list.push(b)
+//             console.log(elem)
+//         // }
+//     })
+//     fs.writeFile(`GenesisComent${l}.json`, JSON.stringify(list), err => {
+//         if (err) console.error(err);
+//     });
+//     // console.log(list);
+// }
+// setTimeout(() => {
+//     for (var i = 1; i <= 2; i++){
+//                 getComment(i)
+//                 console.log(i)
+//             }
+//     }, 5000)
